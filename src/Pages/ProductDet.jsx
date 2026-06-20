f8b0f2ad6df9baad22af63a95b7b1d05b5cb3b28
Musab Dholayare <musab@gmail.com>
2026-06-20 15:14:02 +0100

    adding reponsive

--- src/Pages/ProductDet.jsx
+++ src/Pages/ProductDet.jsx
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom";
 export default function ProductDet() {
   return (
     <>
+    <Link to="/">
     <h2>This is Product Detail</h2>
+    </Link>
     </>
   )
 }
